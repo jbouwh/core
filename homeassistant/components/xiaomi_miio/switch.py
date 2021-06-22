0a36ff25a15e2d3a83eb77ce11278a9b7babf65c
jbouwh <jan@jbsoft.nl>
2021-06-22 13:00:45 +0000

    Implement buzzer, child_lock and dry mode

--- homeassistant/components/xiaomi_miio/switch.py
+++ homeassistant/components/xiaomi_miio/switch.py
@@ -157,7 +157,11 @@ SERVICE_TO_METHOD = {
 
 @dataclass
 class SwitchType:
+<<<<<<< HEAD
     """Class that holds device specific info for a xiaomi aqara or humidifiers."""
+=======
+    """Class that holds device specific info for a xiaomi aqara or humidifier selectors."""
+>>>>>>> Implement buzzer, child_lock and dry mode
 
     name: str = None
     short_name: str = None
@@ -388,9 +392,13 @@ class XiaomiGenericCoordinatedSwitch(XiaomiCoordinatedMiioEntity, SwitchEntity):
     @property
     def is_on(self):
         """Return the current option."""
+<<<<<<< HEAD
         if self.available:
             return self._state
         return None
+=======
+        return self._state
+>>>>>>> Implement buzzer, child_lock and dry mode
 
     @staticmethod
     def _extract_value_from_attribute(state, attribute):
